78b4e60e021a58462a05ed6a6289f2d1ba96c82b
alejo7053 <alejo7053@hotmail.com>
2015-08-13 12:55:42 -0500

    Adicion de codigo en main.c

--- calculadora.h
+++ calculadora.h
@@ -1,6 +1,4 @@
-int suma(int a, int b); 
- 
-int resta(int a, int b); 
- 
-int multiplacion(int a, int b);  
+int suma(int a, int b);
+int resta(int a, int b);
+int multiplacion(int a, int b);
 int division(int a, int b);
--- main.c
+++ main.c
@@ -3,5 +3,29 @@
 
 int main(void)
 {
-  return 0;
+	int op, a, b, r;
+	printf("Escoja la operacion a realizar:\n\t1. Suma\n\t2. Resta\n\t3. Multiplicacion\n\t4. Division\n");
+	scanf("%d", &op);
+	printf("Ingrese el numero:\t");
+	scanf("%d", &a);
+	printf("Ingrese el numero:\t");
+	scanf("%d", &a);
+	switch(op)
+	{
+	case 1:
+		r=suma(a,b);
+		printf("El resultado es:\t %d", r);
+	case 2:
+		r=resta(a,b);
+		printf("El resultado es:\t %d", r);
+	case 3:
+		r=multiplicacion(a,b);
+		printf("El resultado es:\t %d", r);
+	case 4:
+		r=division(a,b);
+		printf("El resultado es:\t %d", r);
+	default:
+		printf("Error!");
+	}
+	return 0;
 }
